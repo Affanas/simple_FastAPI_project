92f38803eaa7fcecca9f7fe7b9436f468a0c3187
Affanas <113759858+Affanas@users.noreply.github.com>
2024-01-27 09:40:28 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from src import db.py
+import s
 
 app = FastAPI()
 
@@ -37,3 +38,12 @@ async def hello():
 async def create_user(user: User):
     user_id = await database.add_user(user)
     return JSONResponse(content={"user_id": user_id}, status_code=200)
+    connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+
+# Добавляем нового пользователя
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
+
+# Сохраняем изменения и закрываем соединение
+connection.commit()
+connection.close()
