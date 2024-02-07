392ccead3b9bc2ac11f235503e71073a3554dae2
Affanas <113759858+Affanas@users.noreply.github.com>
2024-02-07 22:15:31 +0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,21 +1,53 @@
-#from typing import Union
-#from pydantic import BaseModel
-from time import sleep
-import sqlite3
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from typing import List, Optional
 
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-login TEXT NOT NULL,
-time_total INTEGER,
-total INTEGER,
-token INTEGER,
-)
-''')
+from sqlalchemy import create_engine
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))
+    password: Mapped[Optional[str]]
+
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.id} {self.name} {self.password}"
+
+    def __repr__(self) -> str:
+        return f"{self.id} {self.name} {self.password}"
+
+
+class Address(Base):
+    __tablename__ = "address_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str]
+
+    user: Mapped["User"] = relationship(back_populates="addresses")
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
+
+    def __str__(self) -> str:
+        return f"{self.id} {self.email}"
+
+    def __repr__(self) -> str:
+        return f"{self.id} {self.email}"
+
+
+engine = create_engine("sqlite:///test.db")
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
 
 
 
